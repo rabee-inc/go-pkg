2d5be9d7dcc6bc9df7bc7e8c44b362dcd4ef6c77
aikizoku <>
2020-10-02 16:05:47 +0900

    oops

--- basicauth/middleware.go
+++ basicauth/middleware.go
@@ -13,13 +13,13 @@ type Middleware struct {
 func (m *Middleware) Handle(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		user, password, ok := r.BasicAuth()
-		if ok {
+		if !ok {
 			w.Header().Set("WWW-Authenticate", "Basic")
 			w.WriteHeader(http.StatusUnauthorized)
 			http.Error(w, "basic auth required.", http.StatusUnauthorized)
 			return
 		}
-		if m.Account.User == user && m.Account.Password == password {
+		if m.Account.User != user || m.Account.Password != password {
 			http.Error(w, "basic auth error.", http.StatusUnauthorized)
 			return
 		}
