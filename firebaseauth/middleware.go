55b4e6a54dfb2fea2c109bd9756dff7c852d75ce
aikizoku <>
2021-02-13 11:27:42 +0900

    firebase auth に オプショナル設定を追加

--- firebaseauth/middleware.go
+++ firebaseauth/middleware.go
@@ -11,7 +11,8 @@ import (
 
 // Middleware ... Firebase認証のミドルウェア
 type Middleware struct {
-	Svc Service
+	svc      Service
+	optional bool
 }
 
 // Handle ... Firebase認証をする
@@ -19,27 +20,29 @@ func (m *Middleware) Handle(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		ctx := r.Context()
 
-		// Headerを取得
-		ah := r.Header.Get("Authorization")
-		if ah == "" {
-			m.renderError(ctx, w, http.StatusForbidden, "no Authorization header")
-			return
+		if !m.optional {
+			// Headerを取得
+			ah := r.Header.Get("Authorization")
+			if ah == "" {
+				m.renderError(ctx, w, http.StatusForbidden, "no Authorization header")
+				return
+			}
+			ctx = setAuthHeader(ctx, ah)
+
+			// 認証
+			userID, claims, err := m.svc.Authentication(ctx, ah)
+			if err != nil {
+				m.renderError(ctx, w, http.StatusForbidden, err.Error())
+				return
+			}
+
+			// 認証結果を設定
+			ctx = setUserID(ctx, userID)
+			log.Debugf(ctx, "UserID: %s", userID)
+
+			ctx = setClaims(ctx, claims)
+			log.Debugf(ctx, "Claims: %v", claims)
 		}
-		ctx = setAuthHeader(ctx, ah)
-
-		// 認証
-		userID, claims, err := m.Svc.Authentication(ctx, ah)
-		if err != nil {
-			m.renderError(ctx, w, http.StatusForbidden, err.Error())
-			return
-		}
-
-		// 認証結果を設定
-		ctx = setUserID(ctx, userID)
-		log.Debugf(ctx, "UserID: %s", userID)
-
-		ctx = setClaims(ctx, claims)
-		log.Debugf(ctx, "Claims: %v", claims)
 
 		next.ServeHTTP(w, r.WithContext(ctx))
 	})
@@ -51,8 +54,9 @@ func (m *Middleware) renderError(ctx context.Context, w http.ResponseWriter, sta
 }
 
 // NewMiddleware ... Middlewareを作成する
-func NewMiddleware(svc Service) *Middleware {
+func NewMiddleware(svc Service, optional bool) *Middleware {
 	return &Middleware{
-		Svc: svc,
+		svc:      svc,
+		optional: optional,
 	}
 }
--- go.mod
+++ go.mod
@@ -3,11 +3,11 @@ module github.com/rabee-inc/go-pkg
 go 1.14
 
 require (
-	cloud.google.com/go v0.75.0
+	cloud.google.com/go v0.76.0
 	cloud.google.com/go/bigquery v1.15.0
 	cloud.google.com/go/firestore v1.4.0
-	cloud.google.com/go/pubsub v1.9.1
-	cloud.google.com/go/storage v1.12.0
+	cloud.google.com/go/pubsub v1.10.0
+	cloud.google.com/go/storage v1.13.0
 	firebase.google.com/go v3.13.0+incompatible
 	github.com/davecgh/go-spew v1.1.1
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
@@ -22,14 +22,12 @@ require (
 	github.com/stretchr/testify v1.7.0 // indirect
 	github.com/unrolled/render v1.0.3
 	github.com/vincent-petithory/dataurl v0.0.0-20191104211930-d1553a71de50
-	golang.org/x/net v0.0.0-20210119194325-5f4716e94777 // indirect
-	golang.org/x/oauth2 v0.0.0-20210126194326-f9ce19ea3013 // indirect
+	go.opencensus.io v0.22.6 // indirect
+	golang.org/x/oauth2 v0.0.0-20210210192628-66670185b0cd // indirect
 	golang.org/x/sync v0.0.0-20201207232520-09787c993a3a
-	golang.org/x/sys v0.0.0-20210124154548-22da62e12c0c // indirect
 	golang.org/x/text v0.3.5
-	golang.org/x/tools v0.1.0 // indirect
-	google.golang.org/api v0.37.0
-	google.golang.org/genproto v0.0.0-20210126160654-44e461bb6506
+	google.golang.org/api v0.40.0
+	google.golang.org/genproto v0.0.0-20210212180131-e7f2df4ecc2d
 	google.golang.org/grpc v1.35.0
 	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
 	gopkg.in/go-playground/assert.v1 v1.2.1 // indirect
