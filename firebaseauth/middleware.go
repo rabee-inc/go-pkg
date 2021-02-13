5c5430d5a059f8cbd386abca239275e755ac312a
aikizoku <>
2021-02-13 14:00:23 +0900

    実装

--- firebaseauth/middleware.go
+++ firebaseauth/middleware.go
@@ -20,29 +20,31 @@ func (m *Middleware) Handle(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		ctx := r.Context()
 
-		if !m.optional {
-			// Headerを取得
-			ah := r.Header.Get("Authorization")
-			if ah == "" {
+		// Headerを取得
+		ah := r.Header.Get("Authorization")
+		if ah == "" {
+			if !m.optional {
 				m.renderError(ctx, w, http.StatusForbidden, "no Authorization header")
 				return
 			}
-			ctx = setAuthHeader(ctx, ah)
-
-			// 認証
-			userID, claims, err := m.svc.Authentication(ctx, ah)
-			if err != nil {
-				m.renderError(ctx, w, http.StatusForbidden, err.Error())
-				return
-			}
-
-			// 認証結果を設定
-			ctx = setUserID(ctx, userID)
-			log.Debugf(ctx, "UserID: %s", userID)
-
-			ctx = setClaims(ctx, claims)
-			log.Debugf(ctx, "Claims: %v", claims)
+			next.ServeHTTP(w, r.WithContext(ctx))
+			return
 		}
+		ctx = setAuthHeader(ctx, ah)
+
+		// 認証
+		userID, claims, err := m.svc.Authentication(ctx, ah)
+		if err != nil {
+			m.renderError(ctx, w, http.StatusForbidden, err.Error())
+			return
+		}
+
+		// 認証結果を設定
+		ctx = setUserID(ctx, userID)
+		log.Debugf(ctx, "UserID: %s", userID)
+
+		ctx = setClaims(ctx, claims)
+		log.Debugf(ctx, "Claims: %v", claims)
 
 		next.ServeHTTP(w, r.WithContext(ctx))
 	})
