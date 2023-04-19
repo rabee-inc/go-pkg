77b181693a97bd289c76aa739b1f847679edd2b0
simiraaaa <gmcthmy@gmail.com>
2023-04-19 15:30:35 +0900

    変更検知方法を修正

--- util/util.go
+++ util/util.go
@@ -1,10 +1,11 @@
 package util
 
-// AssignIfNotNil ... src が nil でない場合に dest に代入する
-func AssignIfNotNil[T any](dest *T, src *T) bool {
+// AssignIfNotNil ... src が nil でない場合に dest に代入する。代入前の値を含むpointerを返す
+func AssignIfNotNil[T any](dest *T, src *T) *T {
 	if src != nil {
+		old := *dest
 		*dest = *src
-		return true
+		return &old
 	}
-	return false
+	return dest
 }
