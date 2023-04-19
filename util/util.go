ee47be0af14c5635641d48e6e9874f31c1d59487
simiraaaa <gmcthmy@gmail.com>
2023-04-19 14:33:23 +0900

    AssignIfNotNil で代入したときにtrueを返す

--- util/util.go
+++ util/util.go
@@ -1,8 +1,10 @@
 package util
 
 // AssignIfNotNil ... src が nil でない場合に dest に代入する
-func AssignIfNotNil[T any](dest *T, src *T) {
+func AssignIfNotNil[T any](dest *T, src *T) bool {
 	if src != nil {
 		*dest = *src
+		return true
 	}
+	return false
 }
