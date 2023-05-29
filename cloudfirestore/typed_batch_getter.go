d0b08462a22cc9b9f717423dc31f0dde8863083c
simiraaaa <gmcthmy@gmail.com>
2023-05-29 13:15:06 +0900

    fix

--- cloudfirestore/typed_batch_getter.go
+++ cloudfirestore/typed_batch_getter.go
@@ -1,11 +1,5 @@
 package cloudfirestore
 
-import (
-	"cloud.google.com/go/firestore"
-)
-
-type FuncGetDoc func(ids ...string) *firestore.DocumentRef
-
 type TypedBatchGetter[T any] interface {
 	ConvertibleBatchGetter[T, T]
 }
