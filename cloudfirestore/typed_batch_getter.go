64695309bcb0a2c0947bccbd5c322a2c4b8c7f6e
simiraaaa <gmcthmy@gmail.com>
2023-05-29 13:13:34 +0900

    TypedBatchGetterをConvertibleBatchGetterと同じ仕様にする

--- cloudfirestore/typed_batch_getter.go
+++ cloudfirestore/typed_batch_getter.go
@@ -1,17 +1,11 @@
 package cloudfirestore
 
 import (
-	"context"
-
 	"cloud.google.com/go/firestore"
 )
 
 type FuncGetDoc func(ids ...string) *firestore.DocumentRef
 
 type TypedBatchGetter[T any] interface {
-	Add(ids ...string)
-	Delete(ids ...string)
-	GetMap() map[string]*T
-	Get(ids ...string) *T
-	Commit(ctx context.Context) error
+	ConvertibleBatchGetter[T, T]
 }
